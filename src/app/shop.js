5d7ff790855c5250cb29a9ff59fc8670932c5ead
JTP-programming <cinammonseal988@gmail.com>
2025-06-13 08:52:44 +0000

    Update

---
+++ src/app/shop.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Shop() {
+  return (
+    <div>
+      <div>
+        <Image
+          src="/logo.png"
+          alt="Google Logo"
+          width={272}
+          height={92}
+          className="Google_Logo"
+        ></Image>
+      </div>
+
+      <input type="text" placeholder="  🔍  Search Google or Type URL"></input>
+      <div>
+        <button className="Google_Search">Google Search</button>
+        <button className="Google_Lucky">I'm Feeling Lucky</button>
+      </div>
+      <div></div>
+    </div>
+  );
+}
